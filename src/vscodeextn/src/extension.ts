6c49cc84d8b89a3a8889c116250b5ccc27be6df1
stewart <stewart_robertson@sky.com>
2017-03-21 20:45:57 +0000

    wip

--- src/vscodeextn/src/extension.ts
+++ src/vscodeextn/src/extension.ts
@@ -1,6 +1,8 @@
 'use strict';
 import * as vscode from 'vscode';
-import { TypewriterLanguageProvider } from './TypewriterLanguageProvider'
+import { TypewriterLanguageProvider } from './TypewriterLanguageProvider';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const disposables: vscode.Disposable[] = [];
 
@@ -10,17 +12,30 @@ export function activate(context: vscode.ExtensionContext) {
 
     console.log('activating...');
 
-    let invokationDisposable = vscode.commands.registerCommand('extension.showOutput', () => {
+    let invocationDisposable = vscode.commands.registerCommand('extension.showOutput', () => {
         return vscode.commands.executeCommand('vscode.previewHtml', windowUri, vscode.ViewColumn.Two, 'output').then((success) => {
         }, (reason) => {
             vscode.window.showErrorMessage("There's been an error: " + reason);
         });
     });
-    disposables.push(invokationDisposable);
+    disposables.push(invocationDisposable);
 
     let provider = new TypewriterLanguageProvider(windowUri);
     let providerDisposable = vscode.workspace.registerTextDocumentContentProvider(TypewriterLanguageProvider.Scheme, provider);
     disposables.push(providerDisposable);
+
+    const fullPath = path.join(vscode.extensions.getExtension("stewart-r.vs-code-typewriter").extensionPath) + "/server/ilViewer.WebApi.dll";//TODO
+    fs.exists(fullPath, function(exists){
+        if (!exists){
+            console.log(`Unable to start server, can't find path at ${fullPath}`);
+            vscode.window.showErrorMessage("Unable to start server, check developer console");
+            return;
+        }
+        
+        //startServer(fullPath);
+    });
+
+    context.subscriptions.push(...disposables);
 }
 
 // this method is called when your extension is deactivated
